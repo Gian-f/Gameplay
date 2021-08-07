374ef8dd835e2df414e7ebbc4de1248f1346b483
Gian-f <gianfelipe87@gmail.com>
2021-08-07 09:47:00 -0300

    Implementando appointment e appointment details

---
+++ src/screens/AppointmentDetails/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Background } from '../../components/Background';
+
+import { View } from 'react-native';
+
+ export function AppointmentDetails(){
+  return (
+    <Background>
+
+    </Background>
+  );
+}
